6a82d488a31d196cc900962b4497631e92c08a24
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-02-12 17:09:09 +0300

    Inverse_warping_and_geometric_transformation_using_the_warp()_function.py

---
+++ Inverse_warping_and_geometric_transformation_using_the_warp()_function.py
@@ -0,0 +1,12 @@
+import cv2
+from skimage import io
+import numpy as np
+from matplotlib import pyplot as plt
+from skimage.transform import swirl, SimilarityTransform, warp
+
+im = io.imread("C:/Users/aslan/PycharmProjects/openCV/images/yzx.jpg")
+tform = SimilarityTransform(scale=0.9,
+rotation=np.pi/4,translation=(im.shape[0]/2, -100))
+warped = warp(im, tform)
+import matplotlib.pyplot as plt
+plt.imshow(warped), plt.axis('off'), plt.show()
