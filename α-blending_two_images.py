78a91679b1c87983f049aae6f61b3981074bebd0
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-02-12 17:23:30 +0300

    α-blending_two_images.py

---
+++ α-blending_two_images.py
@@ -0,0 +1,18 @@
+import cv2
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+im1 = Image.open("C:/Users/aslan/PycharmProjects/openCV/images/yz.png")
+im2 = Image.open("C:/Users/aslan/PycharmProjects/openCV/images/me.png").convert('RGB').resize((im1.width, im1.height))
+
+im1 = im1.convert('RGBA')
+im2 = im2.convert('RGBA')
+
+im = Image.blend(im1, im2, alpha=0.5)
+
+plt.figure(figsize=(10, 10))
+plt.imshow(im)
+plt.axis('off')
+plt.title("Blended Image")
+plt.show()
