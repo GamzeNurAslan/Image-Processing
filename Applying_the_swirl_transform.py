79c1c62fd6dbcd21d1990f41cd73c9d25c55185f
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-02-12 17:37:35 +0300

    Applying_the_swirl_transform.py

--- Applying_the_swirl_transform.py
+++ Applying_the_swirl_transform.py
@@ -3,7 +3,7 @@ from skimage import io
 from matplotlib import pyplot as plt
 from skimage.transform import swirl
 
-im = io.imread("C:/Users/aslan/PycharmProjects/openCV/images/yzx.jpg")
+im = io.imread("../images/yzx.jpg")
 swirled = swirl(im, rotation=0, strength=15, radius=200)
 plt.imshow(swirled)
 plt.axis('off')
