dd7f8887dd8293bbde3f9b13d1aa6fbefece19a9
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-02-12 17:04:35 +0300

    Adding_random_Gaussian_noise_to_images.py

---
+++ Adding_random_Gaussian_noise_to_images.py
@@ -0,0 +1,17 @@
+from skimage.util import img_as_float, random_noise
+import cv2
+from skimage import io
+import numpy as np
+from matplotlib import pyplot as plt
+
+im = img_as_float(io.imread("C:/Users/aslan/PycharmProjects/openCV/images/yzx.jpg"))
+plt.figure(figsize=(15,12))
+sigmas = [0.1, 0.25, 0.5, 1]
+for i in range(4):
+    noisy = random_noise(im, var=sigmas[i]**2)
+    plt.subplot(2,2,i+1)
+    plt.imshow(noisy)
+    plt.axis('off')
+plt.title('Gaussian noise with sigma=' + str(sigmas[i]), size=20)
+plt.tight_layout()
+plt.show()
