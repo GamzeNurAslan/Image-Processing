7e9c72d91a53bdb3e28e13aa7705e95f1b30deed
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-02-12 17:35:04 +0300

    Plotting_the_histograms_of_pixel_values_for_the_RGB_channels_of_an_image.py

--- Plotting_the_histograms_of_pixel_values_for_the_RGB_channels_of_an_image.py
+++ Plotting_the_histograms_of_pixel_values_for_the_RGB_channels_of_an_image.py
@@ -3,7 +3,7 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
-im1 = Image.open("C:/Users/aslan/PycharmProjects/openCV/images/yz.png")
+im1 = Image.open("../images/yz.png")
 
 pl = im1.histogram()
 plt.bar(range(256), pl[:256], color='r', alpha=0.5)
