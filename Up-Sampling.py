8d5f01147db6d3e119254cc43302ac9cbf94d94c
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-02-13 20:45:00 +0300

    Up-Sampling.py

--- Up-Sampling.py
+++ Up-Sampling.py
@@ -26,4 +26,26 @@ plt.figure(figsize=(10,10)), plt.imshow(im1), plt.show()
 
 #----------------------------------------#
 
+"""Bir resmi 10 kat büyütüp hem PIL.Image.show() ile gösterir,
+hem de büyütülmüş resmi matplotlib kullanarak ekranda görselleştirir"""
+import cv2
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+im = Image.open("C:/Users/aslan/PycharmProjects/openCV/images/me.png")
+
+resized_im = im.resize((im.width*10, im.height*10), Image.BICUBIC)
+
+resized_im.show()
+
+im_np = np.array(resized_im)
+
+plt.figure(figsize=(10,10))
+plt.imshow(im_np)
+plt.axis('off')  # Eksenleri kaldırır
+plt.show()
+
+#-----------------------------------------#
+
 
